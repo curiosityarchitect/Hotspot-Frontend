615cfbcd6f0ec843f073641ff261b0c89cac4500
Arnob Kazi <shahirmuktasid22@gmail.com>
2022-10-06 19:52:40 -0400

    frontend setup

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,1 +1,19 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import EventScreen from '../screens/events';
+import HomeScreen from '../screens/home';
+import GroupScreen from '../screens/groups';
+
+
+const Tab = createBottomTabNavigator();
+
+export default function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Events" component={EventScreen} />
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Group" component={GroupScreen} />
+    </Tab.Navigator>
+  );
+}
---
+++ src/screens/groups/index.js
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function GroupScreen() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Groups</Text>
+      </View>
+    );
+}
