498620944bf2c1b9428e720c1290bb4531b27748
fuzzyhappy <student.e.wang@gmail.com>
2022-10-08 01:20:26 -0400

    Added Prettier, added ESLint rules for React Native

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    "react-native/react-native": true,
   },
   extends: [
     'plugin:react/recommended',
@@ -10,12 +11,22 @@ module.exports = {
   overrides: [
   ],
   parserOptions: {
+    "ecmaFeatures": {
+       "jsx": true
+    },
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
   plugins: [
     'react',
+    'react-native'
   ],
   rules: {
+    
+    "react-native/no-unused-styles": 2,
+    "react-native/split-platform-components": 2,
+    "react-native/no-inline-styles": 2,
+    "react-native/no-color-literals": 2,
+    "react-native/no-raw-text": 2,
   },
 };
