dd2b257aaaf925c079fdc4629aadd3f7aa5830ee
fuzzyhappy <student.e.wang@gmail.com>
2022-10-11 01:00:44 -0400

    mapview working on android emulator

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,4 +1,4 @@
-import { View, Text, Button } from 'react-native'
+import { StyleSheet, View, Text, Button } from 'react-native'
 import { useState, useEffect, Component } from 'react';
 import * as Location from 'expo-location';
 import React from 'react';
@@ -6,15 +6,39 @@ import { store } from '../../redux/store/store';
 import MapView from 'react-native-maps';
 import { connect } from 'react-redux';
 
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+});
+
 function HomeScreen() {
+
   if (store.getState().hasLocation) {
-    console.log(store.getState());
-    const longitude = store.getState().location.coords.longitude;
-    const latitude = store.getState().location.coords.latitude;
+    const currLongitude = store.getState().location.coords.longitude;
+    const currLatitude = store.getState().location.coords.latitude;
+    const mapRegion = {
+      longitude: currLongitude,
+      latitude: currLatitude,
+      longitudeDelta: 0.0072,
+      latitudeDelta: 0.0072
+    };
     return ( 
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        
-        <Text>Location: {longitude}, {latitude} </Text>
+      <View style={styles.container}>
+        <MapView style={styles.map} region={mapRegion} />
       </View>
     );
   }
