66a7bc9619284a7773ba0ec13b5fbda1c53ac2df
andrewdavis <davi1457@purdue.edu>
2022-10-12 15:46:18 -0400

    Updated Changes to App.js

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
+import React from 'react';
 import { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import MyTabs from './src/navigation';
 import { onBoot } from './src/utils/app.util';
 import { Provider } from 'react-redux';
 import { store } from './src/redux/store/store';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './src/screens/login';
+import WelcomeScreen from './src/screens/welcome';
+import RegisterScreen from './src/screens/register'
+import EventCreationScreen from './src/screens/eventCreation';
+import UserSearchScreen from './src/screens/search';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+const App = () => {
 
   useEffect(() => {
     onBoot();
@@ -14,8 +23,17 @@ export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <MyTabs />
+        <Stack.Navigator headerMode="false" initialRouteName="Welcome">
+        <Stack.Screen name="Welcome" component={WelcomeScreen}/>
+        <Stack.Screen name="Login" component={LoginScreen}/>
+        <Stack.Screen name="Register" component={RegisterScreen}/>
+        <Stack.Screen name="Home" component={MyTabs}/>
+        <Stack.Screen name="Search" component={UserSearchScreen}/>
+        <Stack.Screen name="CreateEvent" component={EventCreationScreen}/>
+      </Stack.Navigator>
       </NavigationContainer>
     </Provider>
   );
-}
+};
+
+export default App;
