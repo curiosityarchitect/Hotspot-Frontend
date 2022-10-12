333124a9b62742ec4d7e0858ae1bb9725028a630
andrewdavis <davi1457@purdue.edu>
2022-10-12 15:51:47 -0400

    Added create event button to event screen

--- src/screens/events/index.js
+++ src/screens/events/index.js
@@ -1,9 +1,32 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity,  StyleSheet } from 'react-native'
 
-export default function EventScreen() {
+const EventScreen = ({navigation}) => {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Events!</Text>
+
+      <TouchableOpacity onPress={()=>navigation.navigate("CreateEvent")} style={styles.createBtn}>
+        <Text style={styles.createText}>Create Event</Text>
+      </TouchableOpacity>
+
       </View>
     );
 }
+
+const styles = StyleSheet.create({
+  createBtn: {
+    width: '30%',
+    borderRadius: 10,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+    backgroundColor: '#000000',
+  },
+
+  createText: {
+    color: 'white',
+  },
+});
+
+export default EventScreen;
