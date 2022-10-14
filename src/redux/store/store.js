69c71ae1998d7d7dc9b95da815f7f6718792b232
andrewdavis <davi1457@purdue.edu>
2022-10-14 12:51:02 -0400

    Updating LoginBranch

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,6 +1,13 @@
 import appReducer from "../reducers/appReducer";
 import { configureStore } from "@reduxjs/toolkit";
 
+const initialState = {
+    foregroundPerm: false,
+    backgroundPerm: false,
+    mapEvents: []
+}
+
 export const store = configureStore({
-    reducer: appReducer
+    reducer: appReducer,
+    preloadedState: initialState
 });
