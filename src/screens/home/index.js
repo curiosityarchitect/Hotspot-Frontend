b40e638c9e711cfc112f02acff3258c9507f5937
fuzzyhappy <student.e.wang@gmail.com>
2022-10-13 23:42:54 -0400

    Finished event markers

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,6 +1,6 @@
 import { View, Text, StyleSheet } from 'react-native'
 import { store } from '../../redux/store/store';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 import { connect, useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { setNearbyEvents } from '../../services/events.service';
@@ -24,6 +24,21 @@ const homeStyles = StyleSheet.create({
   },
 });
 
+const createEventMarkers = (events) => {
+  const markers = events.map((event, index) => {
+    return <Marker
+      key = {index}
+      coordinate = {{
+          longitude: event.location.coordinates[0],
+          latitude: event.location.coordinates[1]
+      }}
+      title = { event.name }
+    />
+  });
+  console.log(markers);
+  return markers;
+}
+
 function MapScreen() {
 
   const dispatch = useDispatch();
@@ -42,15 +57,28 @@ function MapScreen() {
     const mapRegion = {
       longitude: currLongitude,
       latitude: currLatitude,
-      longitudeDelta: 0.0072,
-      latitudeDelta: 0.0072
+      longitudeDelta: 0.01,
+      latitudeDelta: 0.01
     };
     return ( 
       <View style={homeStyles.container}>
         <MapView 
           style={homeStyles.map} 
           initialRegion={mapRegion} 
-          showsUserLocation={true}/>
+          showsUserLocation={true}
+          showsPointsOfInterest = {false}>
+          {console.log(events)}
+          { events.map((event, index) => {
+            return <Marker
+              key = {index}
+              coordinate = {{
+                  longitude: event.location.coordinates[0],
+                  latitude: event.location.coordinates[1]
+              }}
+              title = { event.name }
+            />
+          }) }
+        </MapView>
       </View>
     );
   }
