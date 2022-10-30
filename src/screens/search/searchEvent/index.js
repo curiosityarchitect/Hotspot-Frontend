dce3397053a061f1c62dc47b8bc9e98ff7003959
andrewdavis <davi1457@purdue.edu>
2022-10-30 18:19:02 -0400

    Moved user search and created event search

---
+++ src/screens/search/searchEvent/index.js
@@ -0,0 +1,109 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from 'react-native';
+
+const EventSearchScreen = ({navigation}) => {
+
+  return (
+    <View style={styles.container}>
+
+      <Text style={styles.welcomeText}>Type Event to Search!</Text>
+
+      <StatusBar style="auto" />
+
+      <View style={styles.inputView}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="EventName"
+          placeholderTextColor="#808080"
+        />
+      </View>
+
+      <TouchableOpacity style={styles.addBtn}>
+        <Text style={styles.addText}>Search</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity onPress={()=>navigation.navigate("Home")} style={styles.backBtn}>
+        <Text style={styles.backText}>Back</Text>
+      </TouchableOpacity>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  welcomeText: {
+    marginTop: -30,
+    height: 50,
+    fontSize: 25,
+    color: 'black',
+    fontWeight: 'bold',
+  },
+
+  inputView: {
+    backgroundColor: '#F5F5F5',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#808080',
+    width: '90%',
+    height: 45,
+    marginBottom: 20,
+
+    alignItems: 'center',
+  },
+
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    textAlign: 'center',
+    alignItems: 'center',
+  },
+
+  addBtn: {
+    width: '90%',
+    borderRadius: 10,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+    backgroundColor: '#000000',
+  },
+
+  addText: {
+    color: 'white',
+  },
+
+  backBtn: {
+    width: '90%',
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#808080',
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 10,
+    backgroundColor: '#F5F5F5',
+  },
+  
+  backText: {
+    color: 'black',
+  },
+
+});
+
+export default EventSearchScreen;
