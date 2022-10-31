3cfef7bed4feeb9512da776ceab82b1e68d17e65
andrewdavis <davi1457@purdue.edu>
2022-10-31 18:51:39 -0400

    Trying a different set user

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -1,7 +1,7 @@
 import { backendUrl } from "./const";
 import axios from 'axios';
 
-export async function setUser(username, email, password) {
+/*export async function setUser(username, email, password) {
     if (!username || !email || !password)
         return;
 
@@ -16,6 +16,28 @@ export async function setUser(username, email, password) {
             username: username,
             email: email,
             password: password
+        }
+    })
+    .then((response) => {
+        console.log(response.data);
+    })
+    .catch((err) => {console.log(err)});
+}*/
+
+export async function setUser(username, email, password) {
+    if (!username || !email || !password)
+        return;
+    
+    axios.post(`${backendUrl}/users`, 
+    {
+        username: username,
+        email: email,
+        password: password
+    },
+    {
+        headers: {
+             Accept: 'application/json',
+             'Content-Type': 'application/json'
         }
     })
     .then((response) => {
