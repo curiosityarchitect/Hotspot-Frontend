971131135372bde5f08c10675274ccea6a54974c
andrewdavis <davi1457@purdue.edu>
2022-11-01 18:22:21 -0400

    Updating some files

--- src/screens/eventCreation/create-event-button.js
+++ src/screens/eventCreation/create-event-button.js
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity,  StyleSheet } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
 
 const CreateEventButton = ({navigation}) => {
     return (
--- src/services/events.service.js
+++ src/services/events.service.js
@@ -53,21 +53,3 @@ export async function setEvent(eventName, eventType, eventLocation, startTime, e
     })
     .catch((err) => {console.log(err)});
 }
-
-export async function getEvent() {
-    axios.get(`${backendUrl}/events`, 
-    {
-        method: 'FETCH',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
-        }, 
-        params: { 
-            eventName: eventName
-        }
-    })
-    .then((response) => {
-        console.log(response.data);
-    })
-    .catch((err) => {console.log(err)});
-}
