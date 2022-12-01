d801dcb422b090a63b517c3b5f0972ff0fd2e1da
andrewdavis <davi1457@purdue.edu>
2022-11-30 19:02:06 -0500

    Added icons to navigation tab

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -3,16 +3,59 @@ import EventScreen from '../screens/events';
 import HomeScreen from '../screens/home';
 import GroupScreen from '../screens/groups';
 import ProfileScreen from '../screens/profile';
+import {Icon} from 'react-native-elements';
 
 const Tab = createBottomTabNavigator();
 
 export default function MyTabs() {
   return (
     <Tab.Navigator initialRouteName="Home">
-      <Tab.Screen name="My Events" component={EventScreen} />
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Group" component={GroupScreen} />
-      <Tab.Screen options={{headerShown: false}} name="Profile" component={ProfileScreen}  />
+      <Tab.Screen name="My Events" component={EventScreen}
+      options={{
+            tabBarIcon: () => {
+              return (
+                <Icon
+                name={"description"}
+                color="#000000"
+                />
+              );
+            },
+          }}
+      />
+      <Tab.Screen name="Home" component={HomeScreen}
+      options={{
+            tabBarIcon: () => {
+              return (
+                <Icon
+                name={"map"}
+                color="#000000"
+                />
+              );
+            },
+          }}
+      />
+      <Tab.Screen name="Groups" component={GroupScreen}
+      options={{
+            tabBarIcon: () => {
+              return (
+                <Icon
+                name={"groups"}
+                color="#000000"
+                />
+              );
+            },
+          }}
+      />
+      <Tab.Screen options={{tabBarIcon: () => {
+              return (
+                <Icon
+                name={"person"}
+                color="#000000"
+                />
+              );
+            },
+            headerShown: false
+            }} name="Profile" component={ProfileScreen}  />
     </Tab.Navigator>
   );
 }
